95c3dffbcfa243e2f4867049c30378ab4852d274
nikogenix <nikogenix@gmail.com>
2023-10-14 00:16:49 +0300

    Adjusting endpoints and error handling

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { unknownEndpoint, errorHandler } from "./utils/errorHandling.js";
+import { unknownEndpoint, errorHandler, unsupportedUrl } from "./utils/errorHandling.js";
 import dotenv from "dotenv";
 
 const app = express();
@@ -12,9 +12,35 @@ dotenv.config({ path: "../config/.env" });
 
 const PORT = process.env.API_PORT;
 const URL = process.env.SCRAPING_TEST_URL;
+const local_usage = [`http://localhost:${PORT}/api/url/${URL}`];
 
-app.get(`/api/${URL}`, (req, res) => {
-	res.json({ requested_url: URL });
+const apiUrlPattern =
+	/^(\/api\/url\/)((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9-_.]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9-_.]{2,}\.[a-zA-Z0-9-_.]{2,}\.[a-zA-Z0-9-_.]{2,}(\.[a-zA-Z0-9-_.]{2,})?)/;
+
+const routes = [
+	{
+		path: `/api`,
+		handler: (req, res) => {
+			res.json({ supported: { local_usage, frontend_input: [URL] } });
+		},
+	},
+	{
+		path: `/api/url/${URL}`,
+		handler: (req, res) => {
+			res.json({ requested_url: URL });
+		},
+	},
+	{
+		path: apiUrlPattern,
+		handler: (req, res) => {
+			const url = req.params[1];
+			unsupportedUrl(url, req, res);
+		},
+	},
+];
+
+routes.forEach((route) => {
+	app.get(route.path, route.handler);
 });
 
 app.use(unknownEndpoint);
--- api/utils/errorHandling.js
+++ api/utils/errorHandling.js
@@ -3,14 +3,22 @@ dotenv.config({ path: "../config/.env" });
 
 const PORT = process.env.API_PORT;
 const URL = process.env.SCRAPING_TEST_URL;
+const local_usage = [`http://localhost:${PORT}/api/url/${URL}`];
 
 export const unknownEndpoint = (request, response) => {
-	response
-		.status(404)
-		.send({ error: "unknown endpoint", local_usage: `http://localhost:${PORT}/api/${URL}`, frontend_input: URL });
+	response.status(404).send({ error: "unknown endpoint", supported: { local_usage, frontend_input: [URL] } });
+};
+
+export const unsupportedUrl = (url, request, response) => {
+	response.status(404).send({
+		requested_url: url,
+		error: "unsupported URL",
+		supported: { local_usage, frontend_input: [URL] },
+	});
 };
 
 export const errorHandler = (err, req, res, next) => {
+	console.log(err);
 	if (err.name) return res.status(400).send({ error: err.name });
 	next(err);
 };
