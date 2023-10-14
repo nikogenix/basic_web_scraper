bcc9cd09b16e709d828acbc19f121239bb2bc376
nikogenix <nikogenix@gmail.com>
2023-10-14 03:28:21 +0300

    Updating variable name for clarity. Adding scraper function

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { unknownEndpoint, errorHandler, unsupportedUrl } from "./utils/errorHandling.js";
 import dotenv from "dotenv";
+import { scrapeTestSite } from "./utils/testSiteScraping.js";
 
 const app = express();
 app.use(express.static("build"));
@@ -11,8 +12,8 @@ app.use(cors());
 dotenv.config({ path: "../config/.env" });
 
 const PORT = process.env.API_PORT;
-const URL = process.env.SCRAPING_TEST_URL;
-const local_usage = [`http://localhost:${PORT}/api/url/${URL}`];
+const SITE_URL = process.env.SCRAPING_TEST_URL;
+const local_usage = [`http://localhost:${PORT}/api/url/${SITE_URL}`];
 
 const apiUrlPattern =
 	/^(\/api\/url\/)((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9-_.]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9-_.]{2,}\.[a-zA-Z0-9-_.]{2,}\.[a-zA-Z0-9-_.]{2,}(\.[a-zA-Z0-9-_.]{2,})?)/;
@@ -21,13 +22,14 @@ const routes = [
 	{
 		path: `/api`,
 		handler: (req, res) => {
-			res.json({ supported: { local_usage, frontend_input: [URL] } });
+			res.json({ supported: { local_usage, frontend_input: [SITE_URL] } });
 		},
 	},
 	{
-		path: `/api/url/${URL}`,
-		handler: (req, res) => {
-			res.json({ requested_url: URL });
+		path: `/api/url/${SITE_URL}`,
+		handler: async (req, res) => {
+			const data = await scrapeTestSite(SITE_URL);
+			res.json({ requested_url: SITE_URL, data });
 		},
 	},
 	{
