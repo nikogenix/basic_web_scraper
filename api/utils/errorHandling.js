392e3f2384e9590d673dc10bcf2652c385e406d1
nikogenix <nikogenix@gmail.com>
2023-10-14 03:24:27 +0300

    Updating variable name for clarity

--- api/utils/errorHandling.js
+++ api/utils/errorHandling.js
@@ -2,18 +2,18 @@ import dotenv from "dotenv";
 dotenv.config({ path: "../config/.env" });
 
 const PORT = process.env.API_PORT;
-const URL = process.env.SCRAPING_TEST_URL;
-const local_usage = [`http://localhost:${PORT}/api/url/${URL}`];
+const SITE_URL = process.env.SCRAPING_TEST_URL;
+const local_usage = [`http://localhost:${PORT}/api/url/${SITE_URL}`];
 
 export const unknownEndpoint = (request, response) => {
-	response.status(404).send({ error: "unknown endpoint", supported: { local_usage, frontend_input: [URL] } });
+	response.status(404).send({ error: "unknown endpoint", supported: { local_usage, frontend_input: [SITE_URL] } });
 };
 
 export const unsupportedUrl = (url, request, response) => {
 	response.status(404).send({
 		requested_url: url,
 		error: "unsupported URL",
-		supported: { local_usage, frontend_input: [URL] },
+		supported: { local_usage, frontend_input: [SITE_URL] },
 	});
 };
 
